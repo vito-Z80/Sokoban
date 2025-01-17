f0418113039d26cd126d3255d423cf3e654cd233
Виталий <efiop80@gmail.com>
2025-01-18 04:53:41 +0700

    restartLevel сделал  метод заготовку. Нужно будет убрать сборщик уровня  -пусть появляется как есть, его все равно не будет видно.

--- Sokoban/Assets/Scripts/Level/LevelManager.cs
+++ Sokoban/Assets/Scripts/Level/LevelManager.cs
@@ -4,6 +4,7 @@ using Bridge;
 using Data;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.Rendering.Universal;
 
 namespace Level
 {
@@ -12,6 +13,9 @@ namespace Level
         [SerializeField] CameraManager cameraManager;
         [SerializeField] Assembler electrician;
         [SerializeField] public BridgeDisplay bridge;
+
+        [SerializeField] UniversalRenderPipelineAsset urp;
+
         int m_currentLevelId;
         public Level m_currentLevel;
 
@@ -35,6 +39,8 @@ namespace Level
             try
             {
                 await StartLevelZero();
+                urp.renderScale = 1.0f;
+                urp.upscalingFilter = UpscalingFilterSelection.Linear;
                 QualitySettings.vSyncCount = 0;
                 Application.targetFrameRate = 60;
             }
@@ -72,123 +78,177 @@ namespace Level
             StepsController.OnPop?.Invoke();
         }
 
+        public void LevelRestart()
+        {
+            _ = Restart();
+        }
+
+        async Task Restart()
+        {
+            urp.upscalingFilter = UpscalingFilterSelection.Point;
+            while (urp.renderScale > 0.11f)
+            {
+                urp.renderScale -= Time.deltaTime;
+                await Task.Yield();
+            }
+
+
+            var levelPosition = m_currentLevel.transform.position;
+            var levelRotation = m_currentLevel.transform.rotation;
+            Destroy(m_currentLevel.gameObject);
+            m_currentLevel = null;
+            m_currentLevel = await InstantiateNewLevel(m_currentLevelId);
+            m_currentLevel.transform.position = levelPosition;
+            m_currentLevel.transform.rotation = levelRotation;
+            m_currentLevel.gameObject.SetActive(true);
+
+            Debug.Log("Level Activated");
+            var frontDoorPosition = m_currentLevel.enterDoor.transform.position;
+            var doorForward = m_currentLevel.enterDoor.transform.forward;
+            var doorRotation = m_currentLevel.enterDoor.transform.rotation;
+
+            frontDoorPosition += doorForward;
+            frontDoorPosition.y = -0.5f;
+
+            electrician.transform.position = frontDoorPosition;
+            electrician.targetPosition = frontDoorPosition;
+            electrician.transform.rotation = doorRotation;
+
+            Debug.Log("Electrician started");
+
+            await Task.Yield();
+            foreach (var box in m_currentLevel.GetColoredBoxes())
+            {
+                box.EnableBox();
+            }
+            
+            Debug.Log("SCALE+++");
+
+            while (urp.renderScale < 1.0f)
+            {
+                await Task.Yield();
+                urp.renderScale += Time.deltaTime / 2.0f;
+            }
+            Debug.Log("SCALE DONE");
+
+            urp.renderScale = 1.0f;
+            urp.upscalingFilter = UpscalingFilterSelection.Linear;
+        }
 
         void LevelCompleted()
         {
             _ = ToNextLevel();
         }
 
-        async Task  ToNextLevel()
+        async Task ToNextLevel()
         {
-             //  условия уровня выполнены.
-                Global.Instance.levelPhase = LevelPhase.SolutionFound;
+            //  условия уровня выполнены.
+            Global.Instance.levelPhase = LevelPhase.SolutionFound;
 
-                // TODO 4 уровень не правильно собирается если входная дверь под углом 0 градусов. Напольные кнопки тоже не работают...
-                
-                m_currentLevelId=6;
-                m_stepsController ??= new StepsController(electrician);
-                var nextLevel = await InstantiateNewLevel(m_currentLevelId);
+            // TODO 4 уровень не правильно собирается если входная дверь под углом 0 градусов. Напольные кнопки тоже не работают...
 
-                if (nextLevel.gameObject.activeSelf)
-                {
-                    Debug.LogError($"Level {nextLevel.gameObject.name} is activated. Any level must be deactivated for assembly.");
-                }
-                
-                await nextLevel.DisassembleLevel();
-                var exitDoorPosition = m_currentLevel.exitDoor.transform.position.Round();
-                var exitDoorPoint = exitDoorPosition;
-                exitDoorPoint.y = electrician.transform.position.y;
-                var exitDoorForward = m_currentLevel.exitDoor.transform.forward;
-                //  открыть дверь выхода.
-                m_currentLevel.exitDoor.OpenDoor();
-                //  показать мост.
-                await bridge.Init(exitDoorPosition + Vector3.down + exitDoorForward, nextLevel.enterDoor.transform.forward);
-                
-                //  ждем пока игрок подойдет к выходу.
-                while (Vector3.Distance(exitDoorPoint, electrician.transform.position) > 0.33f)
-                {
-                    await Task.Yield();
-                }
+            m_currentLevelId = 6;
+            m_stepsController ??= new StepsController(electrician);
+            var nextLevel = await InstantiateNewLevel(m_currentLevelId);
+            if (nextLevel.gameObject.activeSelf)
+            {
+                Debug.LogError($"Level {nextLevel.gameObject.name} is activated. Any level must be deactivated for assembly.");
+            }
 
-                //  Забрать управление у игрока.
-                electrician.autoMove = true;
-                //  Уровень завершен (был покинут через дверь выхода).
-                Global.Instance.levelPhase = LevelPhase.Finished;
+            await nextLevel.DisassembleLevel();
+            var exitDoorPosition = m_currentLevel.exitDoor.transform.position.Round();
+            var exitDoorPoint = exitDoorPosition;
+            exitDoorPoint.y = electrician.transform.position.y;
+            var exitDoorForward = m_currentLevel.exitDoor.transform.forward;
+            //  открыть дверь выхода.
+            m_currentLevel.exitDoor.OpenDoor();
+            //  показать мост.
+            await bridge.Init(exitDoorPosition + Vector3.down + exitDoorForward, nextLevel.enterDoor.transform.forward);
 
-                //  Указываем позицию автопилота персонажа. 
-                var stopPosition = (nextLevel.enterDoor.transform.position + nextLevel.enterDoor.transform.forward).RoundWithoutY();
-
-                //  Устанавливаем персонажа на автопилот.
-                electrician.SetAutoMove(stopPosition + Vector3.down * 0.5f, exitDoorForward);
-
-                electrician.SetRightForward();
-                cameraManager.SetFollow();
-                
-                
-                //  Обнулить шаги.
-                Global.Instance.gameState.steps = 0;
-                
-
-                //  закрываем дверь выхода когда игрок прошел эту дверь.
-                while (Vector3.Distance(electrician.transform.position, exitDoorPosition + exitDoorForward) > 0.7f)
-                {
-                    await Task.Yield();
-                }
-
-                m_currentLevel.exitDoor.CloseDoor();
-
-
-                //  Ожидание персонажа, пока не достигнет 3х клеток до входной двери следующего уровня.
-                while (Vector3.Distance(electrician.transform.position, stopPosition - exitDoorForward * 4) > 1.0f)
-                {
-                    await Task.Yield();
-                }
-
-                //  Активируем следующий уровень. При активации начнется его построение.
-                nextLevel.gameObject.SetActive(true);
-
-                var enterDoorTransform = nextLevel.enterDoor.transform;
-                //  открываем дверь входа нового уровня когда игрок подходит к двери.
-                while (Vector3.Distance(electrician.transform.position, enterDoorTransform.position) > 3.0f)
-                {
-                    await Task.Yield();
-                }
-
-                nextLevel.enterDoor.OpenDoor();
-
-                //  спрятать мост.
-                await bridge.Init(exitDoorPosition + Vector3.down + exitDoorForward, nextLevel.enterDoor.transform.forward);
-
-                //  закрываем дверь входа нового уровня когда игрок станет с другой стороны двери
-                while (Vector3.Distance(electrician.transform.position, enterDoorTransform.position + enterDoorTransform.forward) > 1.3f)
-                {
-                    await Task.Yield();
-                }
-
-                nextLevel.enterDoor.CloseDoor();
-                
-
-                //  уничтожить предыдущий уровень.
+            //  ждем пока игрок подойдет к выходу.
+            while (Vector3.Distance(exitDoorPoint, electrician.transform.position) > 0.33f)
+            {
                 await Task.Yield();
-                DestroyImmediate(m_currentLevel.gameObject);
+            }
+
+            //  Забрать управление у игрока.
+            electrician.autoMove = true;
+            //  Уровень завершен (был покинут через дверь выхода).
+            Global.Instance.levelPhase = LevelPhase.Finished;
+
+            //  Указываем позицию автопилота персонажа. 
+            var stopPosition = (nextLevel.enterDoor.transform.position + nextLevel.enterDoor.transform.forward).RoundWithoutY();
+
+            //  Устанавливаем персонажа на автопилот.
+            electrician.SetAutoMove(stopPosition + Vector3.down * 0.5f, exitDoorForward);
+
+            electrician.SetRightForward();
+            cameraManager.SetFollow();
+
+
+            //  Обнулить шаги.
+            Global.Instance.gameState.steps = 0;
+
+
+            //  закрываем дверь выхода когда игрок прошел эту дверь.
+            while (Vector3.Distance(electrician.transform.position, exitDoorPosition + exitDoorForward) > 0.7f)
+            {
                 await Task.Yield();
-                
+            }
+
+            m_currentLevel.exitDoor.CloseDoor();
 
 
-                m_currentLevel = nextLevel;
-                //  Передать управление игроку.
-                electrician.autoMove = false;
-                //  Можно приступить к решению уровня.
-                Global.Instance.levelPhase = LevelPhase.SearchSolution;
+            //  Ожидание персонажа, пока не достигнет 3х клеток до входной двери следующего уровня.
+            while (Vector3.Distance(electrician.transform.position, stopPosition - exitDoorForward * 4) > 1.0f)
+            {
+                await Task.Yield();
+            }
 
-                //  Активировать коробки.
-                foreach (var coloredBox in m_currentLevel.GetColoredBoxes())
-                {
-                    coloredBox.EnableBox();
-                }
+            //  Активируем следующий уровень. При активации начнется его построение.
+            nextLevel.gameObject.SetActive(true);
 
-                //  Получить все MainObject уровня для контроля Undo.
-                m_stepsController.CollectMainObjects(nextLevel.gameObject);
+            var enterDoorTransform = nextLevel.enterDoor.transform;
+            //  открываем дверь входа нового уровня когда игрок подходит к двери.
+            while (Vector3.Distance(electrician.transform.position, enterDoorTransform.position) > 3.0f)
+            {
+                await Task.Yield();
+            }
+
+            nextLevel.enterDoor.OpenDoor();
+
+            //  спрятать мост.
+            await bridge.Init(exitDoorPosition + Vector3.down + exitDoorForward, nextLevel.enterDoor.transform.forward);
+
+            //  закрываем дверь входа нового уровня когда игрок станет с другой стороны двери
+            while (Vector3.Distance(electrician.transform.position, enterDoorTransform.position + enterDoorTransform.forward) > 1.3f)
+            {
+                await Task.Yield();
+            }
+
+            nextLevel.enterDoor.CloseDoor();
+
+
+            //  уничтожить предыдущий уровень.
+            await Task.Yield();
+            DestroyImmediate(m_currentLevel.gameObject);
+            await Task.Yield();
+
+
+            m_currentLevel = nextLevel;
+            //  Передать управление игроку.
+            electrician.autoMove = false;
+            //  Можно приступить к решению уровня.
+            Global.Instance.levelPhase = LevelPhase.SearchSolution;
+
+            //  Активировать коробки.
+            foreach (var coloredBox in m_currentLevel.GetColoredBoxes())
+            {
+                coloredBox.EnableBox();
+            }
+
+            //  Получить все MainObject уровня для контроля Undo.
+            m_stepsController.CollectMainObjects(nextLevel.gameObject);
         }
     }
 }
